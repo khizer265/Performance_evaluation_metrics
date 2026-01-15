0bebfb70b27511e8360d22778e49d05c72674453
khizer265 <69149795+khizer265@users.noreply.github.com>
2026-01-15 12:29:29 +0500

    Add files via upload

---
+++ intern_performance.sql
@@ -0,0 +1,9 @@
+CREATE TABLE intern_performance (
+    intern_id INTEGER,
+    intern_name TEXT,
+    task_name TEXT,
+    completion_time_hours INTEGER,
+    quality_score INTEGER,
+    mentor_feedback INTEGER,
+    submission_date DATE
+);
